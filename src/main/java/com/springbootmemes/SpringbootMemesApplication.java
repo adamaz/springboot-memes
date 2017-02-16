e14f2ed70766dced70bd1a9c46b357aa7956a221
adama_diop_zongo <adamadiopzongo@yahoo.fr>
2017-02-16 01:34:00 -0500

    Initial commit.

---
+++ src/main/java/com/springbootmemes/SpringbootMemesApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmemes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMemesApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringbootMemesApplication.class, args);
+	}
+}
